a171ff166ba6d5cfdcd2a49bab956ae11f8c4ee4
Julien Dehos <dehos@univ-littoral.fr>
2025-07-14 12:21:38 +0200

    update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,18 +1,13 @@
 {
+
   inputs = {
-    ghc-wasm-meta.url = "gitlab:haskell-wasm/ghc-wasm-meta?host=gitlab.haskell.org";
+    miso.url = "github:dmjio/miso";
   };
-  outputs = inputs: inputs.ghc-wasm-meta.inputs.flake-utils.lib.eachDefaultSystem (system:
-    let pkgs = inputs.ghc-wasm-meta.inputs.nixpkgs.legacyPackages.${system};
-    in
-    {
-      devShells.default = pkgs.mkShell {
-        packages = [
-          inputs.ghc-wasm-meta.packages.${system}.all_9_12
-          pkgs.gnumake
-          pkgs.http-server
-        ];
-      };
+
+  outputs = inputs: 
+    inputs.miso.inputs.flake-utils.lib.eachDefaultSystem (system: {
+      devShell = inputs.miso.outputs.devShells.${system}.wasm;
     });
-  }
+
+}
 
