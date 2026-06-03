65f4de7ed4bd6503453105d01d7afd71bb852412
David Johnson <875324+dmjio@users.noreply.github.com>
2026-06-03 13:26:41 -0500

    Use littlest-tokyo as an example. (#5)
    * Use littlest-tokyo as an example.
    
    https://threejs.org/examples/webgl_animation_keyframes.html
    
    This ports the littlest-tokyo example to miso.
    
    * Name tokyo-miso

--- src/FFI.hs
+++ src/FFI.hs
@@ -6,12 +6,11 @@ module FFI
   ) where
 ----------------------------------------------------------------------
 import Miso
-import Control.Monad (void)
 ----------------------------------------------------------------------
 appendInBody :: JSVal -> MisoString -> MisoString -> IO ()
 appendInBody v left top = do
   body <- jsg "document" ! "body"
-  body # "appendChild" $ [v]
+  _ <- body # "appendChild" $ [v]
   vStyle <- v ! "style"
   setField vStyle "left" left
   setField vStyle "top" top
